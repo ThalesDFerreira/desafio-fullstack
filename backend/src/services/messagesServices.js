79321a600553b300dc87852358aa81f62d4c6404
ThalesDFerreira <thalesferreira190@gmail.com>
2023-10-13 04:11:47 -0300

    feat: add route insert message - building 1

--- backend/src/services/messagesServices.js
+++ backend/src/services/messagesServices.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
-// const { message } = require('telegraf/filters')
+const { message } = require('telegraf/filters');
 const Messages = require('../database/models/messages');
 
 const token = process.env.BOT_TOKEN;
 const bot = new Telegraf(token);
 
-let context = [];
+let idClient = '';
+let noInterationAdmin;
 
 const getMessagesServices = async () => {
   const messages = await Messages.findAll();
@@ -15,26 +16,47 @@ const getMessagesServices = async () => {
 
 const insertMessagesServices = async (message) => {
   // const result = await Messages.create({
-  //   name,
   //   message,
   // });
-  bot.on('text', (ctx, next) => {
-    ctx.reply('entrou');
-    next();
-  })
+  noInterationAdmin = false;
+  console.log(noInterationAdmin);
+  await bot.telegram.sendMessage(idClient, `${message}`);
 };
 
 const instanceTelegraf = async () => {
-  bot.start((ctx) => {
+  noInterationAdmin = true;
+  console.log(noInterationAdmin);
+
+  bot.start(async (ctx) => {
     const from = ctx.update.message.from;
-    console.log(from);
-    ctx.reply(`Muito bem-vindo, ${from.first_name}!`);
-    context.push(ctx);
-    console.log(context);
+    idClient = from.id;
+    const messageReceived = ctx.update.message.text;
+    console.log(messageReceived);
+    await ctx.reply(`Muito bem-vindo, ${from.first_name}!`);
+    await ctx.reply('Por favor, digite seu nome completo!');
+    setTimeout(async () => {
+      await ctx.reply('Aguarde alguns minutos e logo será atendido!');
+    }, "4000");
+    // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+    // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+    // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+    // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+    // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+    // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+    // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+    
   });
 
-  bot.startPolling();
-}
+  // if (noInterationAdmin) {
+  //   bot.on(message('text'), async (ctx) => {
+  //     await ctx.reply('Aguarde alguns minutos e logo será atendido!');
+  //     const messageReceived = ctx.update.message.text;
+  //     console.log(messageReceived);
+  //   });
+  // }
+
+  bot.launch();
+};
 
 module.exports = {
   getMessagesServices,
