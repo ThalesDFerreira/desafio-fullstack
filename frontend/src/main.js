eafffca521805729f97d37c3164c62917aba07a8
ThalesDFerreira <thalesferreira190@gmail.com>
2023-10-18 05:00:20 -0300

    fix: tentativa de correção erro hljs.initHighlightingOnLoad();

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import hljs from 'highlight.js';
+
+hljs.initHighlightingOnLoad();
 
 createApp(App).use(router).mount('#app');
